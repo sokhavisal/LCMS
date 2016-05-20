f63d0d3bdaf6c2a410d04b7a40acac1d2f0089ff
sokhavisal <sokha.visal@gmail.com>
2016-05-20 16:03:19 +0700

    webcheck

--- main.php
+++ main.php
@@ -11,6 +11,24 @@
     $_SESSION['last_activity'] = time(); 
     $_SESSION["time_out"] = 10*60*60;
 ?>
-<h1>Main</h1>
-<h3>Well come to my LCMS : Admin <?php echo $_SESSION['admin']; ?></h3> 
+<html>
+    <head>
+	<!--Exinternal link  -->
+	<link rel="stylesheet" type="text/css" href="http://w2ui.com/src/w2ui-1.4.2.min.css" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+	<script type="text/javascript" src="http://w2ui.com/src/w2ui-1.4.2.min.js"></script>
+	<!--Internal link  -->
+	<script type="text/javascript" src="js/w2ui_js.js"></script>
+	<script type="text/javascript" src="js/w2ui-1.4.3.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/w2ui-1.4.3.min.css" />
+	
+    </head>
+    <body>
+	<h1>Main</h1>
+	<h3>Well come to my LCMS : Admin <?php echo $_SESSION['admin']; ?></h3> 
+	<div id="layout" style="width: 100%; height: 500px;"></div>
+    </body>
+</html>
+   
+
 
