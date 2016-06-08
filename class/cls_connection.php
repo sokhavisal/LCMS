d9f9c303fcb8787d6c23ef5a743ad3b02132c910
sokhavisal <sokha.visal@gmail.com>
2016-06-08 10:48:12 +0700

    login

--- class/cls_connection.php
+++ class/cls_connection.php
@@ -16,38 +16,47 @@ class Database{
             PDO::ATTR_PERSISTENT    => true,
             PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
         );
+
         // Create a new PDO instanace
         try{
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
         }
+
         // Catch any errors
         catch(PDOException $e){
             $this->error = $e->getMessage();
         }
     }
+
+
     // prepare stement protect injection sql 
     public function query($query){
 	$this->stmt = $this->dbh->prepare($query);
     }
+
+
     // bind value in pdo use placeholder name or unname placeholder 
     public function bind($param, $value, $type = null){
-	if (is_null($type)) {
-	    switch (true) {
-		case is_int($value):
-		    $type = PDO::PARAM_INT;
-		    break;
-		case is_bool($value):
-		    $type = PDO::PARAM_BOOL;
-		    break;
-		case is_null($value):
-		    $type = PDO::PARAM_NULL;
-		    break;
-		default:
-		    $type = PDO::PARAM_STR;
-	    }
-	}
-	$this->stmt->bindValue($param, $value, $type);
+
+    	if (is_null($type)) {
+    	    switch (true) {
+    		case is_int($value):
+    		    $type = PDO::PARAM_INT;
+    		    break;
+    		case is_bool($value):
+    		    $type = PDO::PARAM_BOOL;
+    		    break;
+    		case is_null($value):
+    		    $type = PDO::PARAM_NULL;
+    		    break;
+    		default:
+    		    $type = PDO::PARAM_STR;
+    	    }
+
+    	}
+    	$this->stmt->bindValue($param, $value, $type);
     }
+    
     // execute query 
     public function execute(){
 	return $this->stmt->execute();
